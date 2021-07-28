b84055627e99775db5a01f2aa5d8bf28dd7af2c3
sgkang <kseungkoo@gmail.com>
2021-07-29 01:51:36 +0900

    feat : 기본설정 추가
    1. board 앱 추가
    2. RestAPI Response 리스트 페이지네이션 설정 추가

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'camp',
+    'board',
     'rest_framework',
+    'django_comment_migrate',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +143,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
