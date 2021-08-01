c2083f8f178a4e3c2624eb6eaba641c82c108a50
sgkang <kseungkoo@gmail.com>
2021-08-02 00:19:21 +0900

    chore : django 배포관련 환경 설정 변경

--- config/settings.py
+++ config/settings.py
@@ -9,22 +9,25 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j3ulpq=(s%*^&uw@gjfd+9o#d27yj-wqkb#@z2ponx(u5_%0lt'
+# SECRET_KEY = 'django-insecure-j3ulpq=(s%*^&uw@gjfd+9o#d27yj-wqkb#@z2ponx(u5_%0lt'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+# DEBUG = True
+DEBUG = os.getenv('DEBUG')
 #접근권한 전체 허용
 ALLOWED_HOSTS = ['*']
 
@@ -142,8 +145,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/var/www/static/',
+# ]
 
 #정적파일 관리에 대한 코드
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
