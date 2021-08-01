3c048a56ce7ede0b9f4428c37f0c6d2ed9de4255
sgkang <kseungkoo@gmail.com>
2021-08-01 17:46:27 +0900

    feat : 기본 라우터 설정
    Swagger 설정 및 게시판,캠핑장정보 기본 라우팅 설정

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from django.conf.urls import url
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="캠핑장 정보 API",
+      default_version='v1',
+      description="전국 무료 캠핑장 정보",
+      # terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="kseungkoo@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('camp/', include('camp.urls')),
+    path('board/', include('board.urls')),
+    # path('rest-auth/', include('rest_auth.urls')), # Login, Logout 관련 기능
+    # path('rest-auth/registration/', include('rest_auth.registration.urls')), # Login, Logout 관련 기능
+
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
