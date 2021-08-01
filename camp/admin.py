dd9e6886228631742e7ba6e8380c6b2383f39731
sgkang <kseungkoo@gmail.com>
2021-08-01 17:44:20 +0900

    feat: admin 페이지 사용자 커스터마이징
    auth 라이브러리 설치시 기본 생성되는 user 테이블을 customize 하기 위해 확장 테이블을 생성 하여 관리자 페이지에 함께 설정 가능하도록 추가

--- camp/admin.py
+++ camp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Site, Location, Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+# accounts.apps.AccountsConfig
 
+
+class ProfileInline(admin.StackedInline) :
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
 # Register your models here.
+
+class CustomizedUserAdmin(UserAdmin) :
+    inlines = (ProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(Site)
+admin.site.register(Location)
+admin.site.register(User, CustomizedUserAdmin)
