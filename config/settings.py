77dd76568ee5f109613468a0ff4814ebd5b47451
sgkang <kseungkoo@gmail.com>
2021-08-04 02:50:56 +0900

    chore : static파일 경로 변경

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR= os.path.join(BASE_DIR, 'static')
+STATIC_DIR= os.path.join(ROOT_DIR, 'static_root')
 
 STATICFILES_DIRS = [
     STATIC_DIR,
