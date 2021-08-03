074a720adb3b4da4afe54032ad71f0a6def77dcb
sgkang <kseungkoo@gmail.com>
2021-08-04 03:25:39 +0900

    chore : 소스 원복

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR= os.path.join(ROOT_DIR, 'static_root')
+STATIC_DIR= os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     STATIC_DIR,
